1a136b582dadebe2aba7552f7f1ee4ed42fd1359
yukachi0808 <shigeyuu06@gmail.com>
2023-03-22 18:03:52 +0900

    add today

--- show.py
+++ show.py
@@ -1,10 +1,6 @@
 import random
 
-def main():
+with open("./hyakunin.txt", encoding="utf-8") as f:
+    wakas = [s.strip() for s in f.readlines()]
 
-    with open("./hyakunin.txt", encoding="utf-8") as f:
-        wakas = [s.strip() for s in f.readlines()]
-        print(wakas[random.randrange(len(wakas))])
-
-if __name__ == '__main__':
-    main()
+print("今日の一句" + wakas[random.randrange(len(wakas))])
